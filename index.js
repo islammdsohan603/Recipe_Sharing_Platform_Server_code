0df7afab5ecd0848a75af519ba05ba75ef80ace0
islammdsohan603 <islammdsohan603@gmail.com>
2026-06-18 21:21:47 +0600

    popular recipe api

---
+++ index.js
@@ -0,0 +1,64 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const express = require("express");
+const cors = require("cors");
+const { configDotenv } = require("dotenv");
+
+const app = express();
+
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGO_DB_URI;
+
+const port = process.env.PORT;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    const database = client.db("recipe");
+    const recipeCollection = database.collection("allrecipe");
+
+    //   popular recipe
+
+    app.get("/api/recipe", async (req, res) => {
+      try {
+        const recipes = await recipeCollection
+          .find({})
+          .sort({ likesCount: -1 })
+          .limit(4)
+          .toArray();
+
+        res.send(recipes);
+      } catch (error) {
+        res.status(500).send({ message: "Server error", error: error.message });
+      }
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+run();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
